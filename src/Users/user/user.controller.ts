1ebc8fdadca788040d6f176743227e74c42b803f
Francisco Jacobo <74878255+NeoLasagna@users.noreply.github.com>
2022-09-29 08:30:27 -0700

    Add files via upload

---
+++ src/Users/user/user.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { User } from 'src/models/User';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+    constructor(private userService : UserService ){
+
+    }
+    @Post()
+    Create(@Body() params : User ):void{
+        this.userService.create(params)
+    }
+    @Get('/all')
+    getUsers(): User[] {
+        return this.userService.getAll()
+    }
+    @Get('/:correo')
+    getUser(@Param('correo') param) : User | String{
+        // Valida la respuesta, si el usuario no existe, regresa un mensaje diciendo que no fue encontrado
+        const user = this.userService.getByEmail(param)
+        return user ?? "El usuario no existe"
+    }
+}
